0d005cc9d1b47efc2709460550e69a5c434245a7
Henrique Melanda <henrique.melanda@C02Y256CJG5H.local>
2019-05-24 14:51:58 -0300

    Page import Lazy and Suspense

---
+++ 25/sistema-pedidos/src/app.js
@@ -0,0 +1,22 @@
+import React, { lazy, Fragment, Suspense } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CssBaseline, LinearProgress } from '@material-ui/core'
+
+const MainPage = lazy(() => import('pages/main'))
+const Login = lazy(() => import('pages/login'))
+
+const App = () => (
+  <Fragment>
+    <CssBaseline />
+    <BrowserRouter>
+      <Suspense fallback={<LinearProgress />}>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/' component={MainPage} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
+  </Fragment>
+)
+
+export default App
---
+++ 25/sistema-pedidos/src/pages/login/login.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import styled from 'styled-components'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { Button, Grid } from '@material-ui/core'
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
+
+const config = {
+  apiKey: 'AIzaSyCvSCUrNh5gK46414T9llzVE08yU_hII70',
+  authDomain: 'reactzzaria-10c3b.firebaseapp.com',
+  databaseURL: 'https://reactzzaria-10c3b.firebaseio.com',
+  projectId: 'reactzzaria-10c3b',
+  storageBucket: 'reactzzaria-10c3b.appspot.com',
+  messagingSenderId: '565150912644'
+}
+
+firebase.initializeApp(config)
+
+let logoutFunction = null
+
+function Login () {
+  const [userInfo, setUserInfo] = useState({
+    isUserLoggedIn: false,
+    user: null
+  })
+
+  const { isUserLoggedIn, user } = userInfo
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setUserInfo({
+        isUserLoggedIn: !!user,
+        user
+      })
+    })
+  }, [])
+
+  const login = useCallback(() => {
+    const provider = new firebase.auth.GithubAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+  }, [])
+
+  const logout = useCallback(() => {
+    firebase.auth().signOut().then(() => {
+      setUserInfo({
+        isUserLoggedIn: false,
+        user: null
+      })
+    })
+  }, [])
+
+  if (!logoutFunction) {
+    logoutFunction = logout
+  }
+
+  console.log(`logoutFunction === logout   ${logoutFunction === logout}`)
+
+  return (
+    <Container>
+      <Grid container justify='center' spacing={40}>
+        <Grid item>
+          <Logo />
+        </Grid>
+        <Grid item xs={12} container justify='center'>
+          {isUserLoggedIn && (
+            <>
+              <pre>{user.displayName}</pre>
+              <Button variant='contained' onClick={logout}>
+                Sair
+              </Button>
+            </>
+          )}
+          {!isUserLoggedIn && (
+            <GithubButton onClick={login}>
+             Entrar com Github
+            </GithubButton>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  padding: 20px;
+`
+const Logo = styled(MainLogo)`
+  width: 100%;
+`
+const GithubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  && {
+    font-size: 25px;
+    max-width: 480px;
+    padding: 15px;
+    text-transform: none;
+  }
+`
+
+export default Login
