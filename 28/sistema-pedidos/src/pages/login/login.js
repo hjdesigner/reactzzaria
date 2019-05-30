8a145507a2eacf11baa56e9b75fd8d4a04b9f773
Henrique Melanda <henrique.melanda@C02Y256CJG5H.local>
2019-05-30 10:17:18 -0300

    Initial structure for use data in the Context API

---
+++ 28/sistema-pedidos/src/pages/login/login.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import styled from 'styled-components'
+import firebase from 'services/firebase'
+import { Button, Grid } from '@material-ui/core'
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
+
+function Login () {
+  const [userInfo, setUserInfo] = useState({
+    isUserLoggedIn: false,
+    user: null
+  })
+
+  const { isUserLoggedIn, user } = userInfo
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setUserInfo({
+        isUserLoggedIn: !!user,
+        user
+      })
+    })
+  }, [])
+
+  const login = useCallback(() => {
+    const provider = new firebase.auth.GithubAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+  }, [])
+
+  const logout = useCallback(() => {
+    firebase.auth().signOut().then(() => {
+      setUserInfo({
+        isUserLoggedIn: false,
+        user: null
+      })
+    })
+  }, [])
+
+  return (
+    <Container>
+      <Grid container justify='center' spacing={40}>
+        <Grid item>
+          <Logo />
+        </Grid>
+        <Grid item xs={12} container justify='center'>
+          {isUserLoggedIn && (
+            <>
+              <pre>{user.displayName}</pre>
+              <Button variant='contained' onClick={logout}>
+                Sair
+              </Button>
+            </>
+          )}
+          {!isUserLoggedIn && (
+            <GithubButton onClick={login}>
+              Entrar com Github
+            </GithubButton>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  padding: 20px;
+`
+const Logo = styled(MainLogo)`
+  width: 100%;
+`
+const GithubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  && {
+    font-size: 25px;
+    max-width: 480px;
+    padding: 15px;
+    text-transform: none;
+  }
+`
+
+export default Login
