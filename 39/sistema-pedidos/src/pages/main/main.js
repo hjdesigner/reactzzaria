ebae48ad911615e81ba97eaf06f6e6684a29e122
Henrique Melanda <henrique.melanda@C02Y256CJG5H.local>
2019-06-16 15:05:24 -0300

    Add hot-loader on CRA

---
+++ 39/sistema-pedidos/src/pages/main/main.js
@@ -0,0 +1,71 @@
+import React, { useState, useContext } from 'react'
+import styled from 'styled-components'
+import {
+  AppBar,
+  Toolbar as MaterialToobar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem
+} from '@material-ui/core'
+import { AccountCircle } from '@material-ui/icons'
+import { ReactComponent as MainLogo } from 'images/logo-react-zzaria.svg'
+import { AuthContext } from 'contexts/auth'
+
+const Main = () => {
+  const [anchorElement, setAnchorElement] = useState(null)
+  const { userInfo, logout } = useContext(AuthContext)
+
+  const handleOpenMenu = (e) => {
+    setAnchorElement(e.target)
+  }
+  const handleClose = () => {
+    setAnchorElement(null)
+  }
+
+  return (
+    <AppBar>
+      <Toolbar>
+        <LogoContiner>
+          <Logo />
+        </LogoContiner>
+
+        <Typography color='inherit'>Olá {userInfo.user.displayName.split(' ')[0]} =)</Typography>
+        <IconButton color='inherit' onClick={handleOpenMenu}>
+          <AccountCircle />
+        </IconButton>
+
+        <Menu
+          open={!!anchorElement}
+          onClose={handleClose}
+          anchorEl={anchorElement}
+        >
+          <MenuItem onClick={logout}>Sair</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+const Toolbar = styled(MaterialToobar)`
+  margin: 0 auto;
+  max-width: 950px;
+  width: 100%;
+`
+
+const LogoContiner = styled.div`
+  flex-grow: 1;
+`
+const Logo = styled(MainLogo)`
+  height: 50px;
+  width: 200px;
+
+  & path {
+    fill: #FFF;
+  }
+  & line {
+    stroke: #FFF;
+  }
+`
+
+export default Main
