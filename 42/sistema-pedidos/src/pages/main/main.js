74ef1256a7eeb35ba44a0ef82157f66dc125b59d
Henrique Melanda <henrique.melanda@C02Y256CJG5H.local>
2019-06-22 17:05:34 -0300

    Content marking of the main page

---
+++ 42/sistema-pedidos/src/pages/main/main.js
@@ -0,0 +1,135 @@
+import React, { useState, useContext, Fragment } from 'react'
+import styled from 'styled-components'
+import {
+  AppBar,
+  Toolbar as MaterialToobar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Grid,
+  withStyles,
+  Paper
+} from '@material-ui/core'
+import { AccountCircle } from '@material-ui/icons'
+import { ReactComponent as MainLogo } from 'images/logo-react-zzaria.svg'
+import { AuthContext } from 'contexts/auth'
+
+const Main = () => {
+  const [anchorElement, setAnchorElement] = useState(null)
+  const { userInfo, logout } = useContext(AuthContext)
+  const userName = userInfo.user.displayName.split(' ')[0]
+
+  const handleOpenMenu = (e) => {
+    setAnchorElement(e.target)
+  }
+  const handleClose = () => {
+    setAnchorElement(null)
+  }
+
+  return (
+    <Fragment>
+      <AppBar>
+        <Toolbar>
+          <LogoContiner>
+            <Logo />
+          </LogoContiner>
+
+          <Typography color='inherit'>Olá {userName} =)</Typography>
+          <IconButton color='inherit' onClick={handleOpenMenu}>
+            <AccountCircle />
+          </IconButton>
+
+          <Menu
+            open={!!anchorElement}
+            onClose={handleClose}
+            anchorEl={anchorElement}
+          >
+            <MenuItem onClick={logout}>Sair</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+
+      <Spacer />
+
+      <Content>
+        <Grid container direction='column' alignItems='center'>
+          <Typography variant='h3'>
+            O que vai ser hoje, {userName}? =)
+          </Typography>
+          <Typography variant='h4'>
+            Escolha o tamanho da pizza:
+          </Typography>
+        </Grid>
+        <Grid container spacing={16}>
+          {pizzaSizes.map((pizza) => (
+            <Grid item key='pizza.id' xs={4}>
+              <Paper style={{ padding: 20 }}>
+                <div>{pizza.size}cm</div>
+                <Typography>{pizza.name}</Typography>
+                <Typography>
+                  {pizza.slices} fatias, {pizza.flavours} sabores
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Content>
+    </Fragment>
+  )
+}
+
+const pizzaSizes = [
+  {
+    id: 0,
+    name: 'Pequena',
+    size: 28,
+    slices: 2,
+    flavours: 1
+  },
+  {
+    id: 1,
+    name: 'Média',
+    size: 30,
+    slices: 6,
+    flavours: 2
+  },
+  {
+    id: 2,
+    name: 'Grande',
+    size: 32,
+    slices: 8,
+    flavours: 3
+  }
+]
+
+const Toolbar = styled(MaterialToobar)`
+  margin: 0 auto;
+  max-width: 950px;
+  width: 100%;
+`
+const LogoContiner = styled.div`
+  flex-grow: 1;
+`
+const Logo = styled(MainLogo)`
+  height: 50px;
+  width: 200px;
+
+  & path {
+    fill: #FFF;
+  }
+  & line {
+    stroke: #FFF;
+  }
+`
+const Content = styled.main`
+  padding: 20px;
+`
+const style = (theme) => ({
+  main: theme.mixins.toolbar
+})
+const Spacer = withStyles(style)(({ classes }) => (
+  <div className={classes.main}>Conteúdo</div>
+))
+
+export default Main
