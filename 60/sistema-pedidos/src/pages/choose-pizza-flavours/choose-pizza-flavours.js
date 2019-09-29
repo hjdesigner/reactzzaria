0f3e3ecc08fbc17b6360cd0a43023f5781844cb5
Henrique Melanda <henrique.melanda@vagas.com.br>
2019-09-29 20:06:28 -0300

    Selection rules of flavors (select right amount)

---
+++ 60/sistema-pedidos/src/pages/choose-pizza-flavours/choose-pizza-flavours.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import t from 'prop-types'
+import { Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+import {
+  Card,
+  Grid,
+  Typography
+} from '@material-ui/core'
+import {
+  H4,
+  HeaderContent,
+  PizzasGrid,
+  Divider,
+  CardActionArea
+} from 'ui'
+import { singularOrPlural } from 'utils'
+import { HOME } from 'routes'
+import pizzasFlavous from 'fake-data/pizzas-flavous'
+
+const ChoosePizzaFlavours = ({ location }) => {
+  const [checkboxes, setCheckboxes] = useState(() => ({}))
+
+  if (!location.state) {
+    return <Redirect to={HOME} />
+  }
+
+  const { flavours, id } = location.state
+
+  const handleChangeCheckbox = (pizzaId) => (e) => {
+    if (
+      checkboxesChecked(checkboxes).length === flavours &&
+      e.target.checked === true
+    ) {
+      return
+    }
+
+    setCheckboxes((checkboxes) => {
+      return {
+        ...checkboxes,
+        [pizzaId]: e.target.checked
+      }
+    })
+  }
+
+  return (
+    <>
+      <HeaderContent>
+        <H4>
+          Escolha até {flavours} {' '}
+          {singularOrPlural(flavours, 'sabor', 'sabores')}:
+        </H4>
+      </HeaderContent>
+
+      <PizzasGrid>
+        {pizzasFlavous.map((pizza) => (
+          <Grid item key={pizza.id} xs>
+            <Card>
+              <Label>
+                <input
+                  type='checkbox'
+                  checked={!!checkboxes[pizza.id]}
+                  onChange={handleChangeCheckbox(pizza.id)}
+                />
+                <Img src={pizza.image} alt={pizza.name} />
+                <Divider />
+                <Typography>{pizza.name}</Typography>
+                <Typography variant='h5'>{pizza.value[id]}</Typography>
+              </Label>
+            </Card>
+          </Grid>
+        ))}
+      </PizzasGrid>
+    </>
+  )
+}
+
+const Img = styled.img`
+  width: 200px;
+`
+const Label = styled(CardActionArea).attrs({
+  component: 'label'
+})``
+
+function checkboxesChecked (checkboxes) {
+  return Object.values(checkboxes).filter(Boolean)
+}
+
+ChoosePizzaFlavours.propTypes = {
+  location: t.object.isRequired
+}
+
+export default ChoosePizzaFlavours
