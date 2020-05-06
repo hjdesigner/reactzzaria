c6c33436e3ba1726cb3b244c92074f57a2d4120c
Henrique Melanda <hjdesigner23@gmail.com>
2020-05-05 23:26:09 -0300

    Class React JS for project reactzzaria

---
+++ 118/sistema-pedidos/src/services/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: 'AIzaSyCvSCUrNh5gK46414T9llzVE08yU_hII70',
+  authDomain: 'reactzzaria-10c3b.firebaseapp.com',
+  databaseURL: 'https://reactzzaria-10c3b.firebaseio.com',
+  projectId: 'reactzzaria-10c3b',
+  storageBucket: 'reactzzaria-10c3b.appspot.com',
+  messagingSenderId: '565150912644'
+}
+
+firebase.initializeApp(config)
+
+export const db = firebase.firestore()
+export default firebase
